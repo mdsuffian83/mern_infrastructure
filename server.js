0888e9948d4f9ea5fd4f8562866e74188b711732
Muhammad Suffian Apandi <mdsuffian83@hotmail.com>
2023-03-23 15:30:25 +0800

    added files

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+require("dotenv").config();
+require("./config/database");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get('/api', (req, res) => {
-  res.json({ msg: 'Hi' });
+app.get("/api", (req, res) => {
+  res.json({ msg: "Hi" });
 });
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(port, () => {
---
+++ src/pages/App/App.jsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <main className="App">App 2</main>;
+}
